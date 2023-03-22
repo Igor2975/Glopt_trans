9f68f551a2c852b858bd5c5972965a070c406744
Igor <a15000a@mail.ru>
2023-03-22 22:25:20 +0300

    валидация формы и номера телефона

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,4 +6,34 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.png"></button>'
     });
-  });
+});
+
+ // ввод телефона
+
+ $('input[name=phone]').mask("+7(999)-999-99-99");
+  
+// //Валидация
+function validateForms (form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Пожалуйста, введите ваше имя",
+        phone: "Пожалуйста, введите ваш номер телефона",
+        email: {
+          required: "Пожалуйста, введите адрес почтового ящика",
+          email: "Адрес почтового ящика введен неверно"
+        }
+      }
+    });
+  }
+
+  validateForms('#request-form');
+validateForms('#consult-form');
+  
